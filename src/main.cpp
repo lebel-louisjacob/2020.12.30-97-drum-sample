cda8a0805a1b108e11ceabba0f9c37c018f9295d
Louis-Jacob Lebel <lebel.louisjacob@gmail.com>
2021-04-10 05:05:01 -0400

    create a dual drum sample

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,44 @@
 #include <musicpp.hpp>
 
+using namespace mpp;
+using namespace mpp::literals;
+
 int main(int const argc, char** const argv)
 {
-    mpp::Config const& config { argc, argv };
+    Config const& config { argc, argv };
+
+    auto const& base_synth
+    {
+        seq
+        (
+            make_basic<SINE>(Bezier { 2_C, 20 }),
+            bind_front<Volume>(Bezier { 1, 0 }),
+            bind_front<Size>(config.sample_rate / 4)
+        )
+    };
+
+    auto const& click_synth
+    {
+        seq
+        (
+            make_basic<SAW>(Bezier { 6_C, 20 }),
+            bind_front<Volume>(Bezier { 0.5, 0 }),
+            bind_front<Size>(config.sample_rate / 32)
+        )
+    };
 
     auto&& input
     {
-        mpp::Mix
+        Volume
         {
-            
+            0.7,
+            Mix
+            {
+                base_synth,
+                click_synth,
+            }
         }
     };
 
-    return mpp::main(input, config);
+    return main(input, config);
 }
